ae7c5a482c10d795277bcfd08cd29453a12938fe
Thiago Silva Azevedo <thiago.dev23@gmail.com>
2025-03-18 19:35:13 -0300

    update all

--- App.js
+++ App.js
@@ -2,14 +2,6 @@ import { View, FlatList, StyleSheet, Text, StatusBar, Image } from 'react-native
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 
 
-const DATA = [
-  
-  {
-    
-    image: 'https://cgi.chevrolet.com/mmgprod-us/dynres/prove/image.gen?i=2025/1YC07/1YC07__2LT/GBA_HTM_0ST_1SZ_2LT_2ST_3ST_4AA_4ST_5A7_5FC_5ST_6X1_719_7X1_8X2_9L3_9X2_A2X_A7K_AH2_AHE_AHH_AJ7_AL0_AL9_AP9_AQA_AT9_AXJ_B72_BAZ_BC7_BGR_CF7_CJ2_D3V_DRZ_DYX_EF7_EFR_EPH_ERI_EYT_FE3_FJW_G96_GM7_IOK_J25_J55_J6A_K4C_KI3_KQV_KRV_LHD_LT2_M1L_MAH_N38_NE1_NE8_NGA_NK4_NKD_NPP_NUB_PPW_PRF_Q9A_R9N_RWL_SLM_T0A_T4L_T8Z_TB8_TDM_TQ5_TTW_U2K_U80_UD7_UDV_UE1_UE4_UEU_UFG_UFT_UG1_UHX_UHY_UJN_UKT_UMN_UQH_UQT_UTJ_UTU_UTV_UV6_UVA_V08_V8D_VA5_VH9_VHM_VK3_VRF_VRG_VRH_VRK_VRL_VRM_VRN_VRR_VT7_VV4_WMZ_XFQ_XL8_Z51gmds2.jpg&v=deg01&std=true&country=US',
-    text: [" Marca: Chevrolet", " Modelo: Corvette", "Ano: 2025"],
-  },
-];
 
 const DATA2 = [
   
@@ -49,25 +41,23 @@ const Card = ({image, text}) => {
   );
 };
 
-const Item = ({ image, text }) => (
+const Item = ({ title, description }) => {
+  return(
   <View style={styles.item}>
-    {image && <Image source={{ uri: image }} style={styles.image} />}
-    {text && text.map((line, index) => <Text key={index} style={styles.text}>{line}</Text>)}
-    
-
+    <Image style={styles.image} source={{uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrd-9Bfj2QOtKuZ1HeKJ1oN1qOg1lqS3SWpA&s'}}></Image>
+    <Text style={styles.text}> {title}</Text>
+    <Text style={styles.description}>{description}</Text>
   </View>
 );
-
+};
 const App = () => (
   <SafeAreaProvider style={styles.fundo}>
 
       <SafeAreaView style={styles.container}>
 
-        <FlatList
-          data={DATA}
-          renderItem={({ item }) => <Item image={item.image} text={item.text} />}
-          keyExtractor={(item) => item.id}
-        />
+      <Text style={styles.headerTitle}>Carros Esportivos!</Text>
+        <Item title="testeeee" description="aaaaaaaaaaaaaaaaaa"/>
+
         <FlatList
           data={DATA2}
           renderItem={({ item }) => <Card image={item.image} text={item.text} />}
@@ -109,7 +99,6 @@ const styles = StyleSheet.create({
     description: {
         fontSize: 12,
         color: 'black',
-        marginLeft: 100,
          
     },
     container: {
@@ -117,7 +106,7 @@ const styles = StyleSheet.create({
       marginTop: StatusBar.currentHeight || 2,
     },
     headerTitle: {
-      color: 'black',
+      color: 'white',
       fontSize: 25,
       margin: 10,
     },
