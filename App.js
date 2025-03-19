679325db37f3458d0e6e7bc85eb1811bb6308ec5
Thiago Silva Azevedo <thiago.dev23@gmail.com>
2025-03-19 02:44:45 -0300

    theme Flamengo

--- App.js
+++ App.js
@@ -7,36 +7,42 @@ const DATA2 = [
   
   {
     
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrd-9Bfj2QOtKuZ1HeKJ1oN1qOg1lqS3SWpA&s',
-    text: ["kkkkkkkkkkk"],
+    image: 'https://i.pinimg.com/736x/af/41/fc/af41fc65f1fa2c145ff00106568afa39.jpg',
+    text: ["Mundial:"],
+    description: ["1x"],
   },
   {
     
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrd-9Bfj2QOtKuZ1HeKJ1oN1qOg1lqS3SWpA&s',
-    text: ["kkkkkkkkkkk"],
+    image: 'https://i.pinimg.com/736x/26/3e/09/263e09a8b6342b65bb8a8bd1f5dbfec5.jpg',
+    text: ["Copa Libertadores:"],
+    description: ["3x"],
   },
   {
     
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrd-9Bfj2QOtKuZ1HeKJ1oN1qOg1lqS3SWpA&s',
-    text: ["kkkkkkkkkkk"],
+    image: 'https://media.gettyimages.com/id/1434873773/pt/foto/rio-de-janeiro-brazil-the-trophy-is-displayed-prior-to-the-second-leg-match-of-the-final-of.jpg?s=612x612&w=0&k=20&c=f4sSzU1nBTWxv2fk1ByFQzkoyiJZoPyLNeqQZNmuobk=',
+    text: ["Copa do Brasil:"],
+    description: ["5x"],
   },
   {
     
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrd-9Bfj2QOtKuZ1HeKJ1oN1qOg1lqS3SWpA&s',
-    text: ["kkkkkkkkkkk"],
+    image: 'https://i.pinimg.com/736x/b3/3a/00/b33a0061f9cb893876fbf32b98520bfc.jpg',
+    text: ["Brasileirão:"],
+    description: ["8x"],
   },
   {
     
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrd-9Bfj2QOtKuZ1HeKJ1oN1qOg1lqS3SWpA&s',
-    text: ["kkkkkkkkkkk"],
+    image: 'https://64.media.tumblr.com/368a8c18d59ca3d1b4b5a267d83c68a5/69932c1a81634c09-0a/s1280x1920/8548570c4ff142b668510c2d52c24a8ce9781c72.pnj',
+    text: ["Campeonato Carioca:"],
+    description: ["39x"],
   },
 ];
 
-const Card = ({image, text}) => {
+const Card = ({image, text, description}) => {
   return(
       <View style={styles.card}>
         {image && <Image source={{ uri: image }} style={styles.source} />}
         {text && text.map((line, index) => <Text key={index} style={styles.title}>{line}</Text>)}
+        {description && description.map((line, index) => <Text key={index} style={styles.description}>{line}</Text>)}
       </View>
   );
 };
@@ -44,9 +50,8 @@ const Card = ({image, text}) => {
 const Item = ({ title, description }) => {
   return(
   <View style={styles.item}>
-    <Image style={styles.image} source={{uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrd-9Bfj2QOtKuZ1HeKJ1oN1qOg1lqS3SWpA&s'}}></Image>
     <Text style={styles.text}> {title}</Text>
-    <Text style={styles.description}>{description}</Text>
+    <Text style={styles.descrição}>{description}</Text>
   </View>
 );
 };
@@ -55,12 +60,13 @@ const App = () => (
 
       <SafeAreaView style={styles.container}>
 
-      <Text style={styles.headerTitle}>Carros Esportivos!</Text>
-        <Item title="testeeee" description="aaaaaaaaaaaaaaaaaa"/>
+      <Text style={styles.headerTitle}>Flamengo títulos</Text>
+      <Image style={styles.image} source={{uri:'https://i.pinimg.com/736x/c7/06/b2/c706b2f7087cc2570cdeabac7e6ca54f.jpg'}}></Image>
+        <Item title="História:" description="O Clube de Regatas do Flamengo, fundado em 17 de novembro de 1895, é um dos maiores e mais populares clubes do Brasil. Inicialmente criado para competições de remo, tornou-se uma potência no futebol, conquistando títulos nacionais e internacionais, incluindo múltiplos Campeonatos Brasileiros e a Copa Libertadores da América. Com uma torcida apaixonada, conhecida como a maior do país, o Flamengo tem como principais cores o vermelho e o preto e manda seus jogos no icônico estádio do Maracanã."/>
 
         <FlatList
           data={DATA2}
-          renderItem={({ item }) => <Card image={item.image} text={item.text} />}
+          renderItem={({ item }) => <Card image={item.image} text={item.text} description={item.description} />}
           keyExtractor={(item) => item.id}
           style={styles.flat}
         />
@@ -80,7 +86,7 @@ const styles = StyleSheet.create({
     },
     card: {
       width: "98%",
-      height:  100,
+      height:  110,
       padding:  20,
       borderRadius: 10,
       borderWidth: 3,
@@ -91,16 +97,33 @@ const styles = StyleSheet.create({
     },
     title: {
         fontSize: 16,
-        marginBottom: 8,
+        marginBottom: 80,
         color: 'black',
-        marginLeft: 100,
-        marginTop: -70,        
+        marginLeft: 80,
+        marginTop: -70,
+        fontWeight: 'bold',        
     },
-    description: {
-        fontSize: 12,
+    descrição: {
+        fontSize: 14,
         color: 'black',
+        marginLeft: 4,
          
     },
+    description: {
+      fontSize: 16,
+      color: 'black',
+      marginLeft: 80,
+      marginTop: -70,
+   
+  },
+  text: {
+    fontSize: 18,
+    marginTop: 0,
+    color: 'black',
+    textAlign: 'left',
+    fontWeight: 'bold', 
+    
+  },
     container: {
       flex: 1,
       marginTop: StatusBar.currentHeight || 2,
@@ -109,6 +132,9 @@ const styles = StyleSheet.create({
       color: 'white',
       fontSize: 25,
       margin: 10,
+      fontWeight: 'bold', 
+      marginLeft: 110,
+      marginTop: -10,
     },
     item: {
       backgroundColor: 'white',
@@ -123,29 +149,21 @@ const styles = StyleSheet.create({
     },
   
     image: {
-      width: "100%",
-      height: 220,
-      resizeMode: 'contain',
-      borderRadius: 10,
-      marginTop: 0,
+      width: "95%",
+      height: 305,
+      borderRadius: 40,
+      margin: 10,
       
-    },
-    text: {
-      fontSize: 18,
-      marginTop: 5,
-      color: '#333',
-      textAlign: 'left',
+
       
     },
     source: {
       width: 70,
-      height: 70,
-      borderRadius: 35,
-      marginTop: -7,
+      height: 90,
+      borderRadius: 30,
+      marginTop: -13,
+      marginLeft: -10,
       
-    },
-    flat: {
-      paddingTop: 20,
     },
 });
 
